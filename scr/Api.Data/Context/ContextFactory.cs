db3f11041370db76b015de0f933ff3d66162aa76
Heriberto F M Komatu <hfmk2015@gmail.com>
2021-08-20 02:42:31 +0900

    problema solucionado, dotnet ef database update ok

--- scr/Api.Data/Context/ContextFactory.cs
+++ scr/Api.Data/Context/ContextFactory.cs
@@ -8,7 +8,7 @@ namespace Api.Data.Context
         public MyContext CreateDbContext(string[] args)
         {
             //Usado para Criar as Migrações
-            var connectionString = "Server=localhost;Port=3306;Database=Course;Uid=root;Pwd=admin";
+            var connectionString = "Server=localhost;Port=3306;Database=Course;Uid=root;Pwd=root";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
             optionsBuilder.UseMySql(connectionString);
             return new MyContext(optionsBuilder.Options);
