bd0dc68d65d3b47d10ded9ea16fe0fbb7093fdf7
Heriberto F M Komatu <hfmk2015@gmail.com>
2021-08-20 23:22:47 +0900

    finalixando hoje a contrução da interface do reposyti

--- scr/Api.Data/Repository/BaseRepository.cs
+++ scr/Api.Data/Repository/BaseRepository.cs
@@ -1,22 +1,60 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Api.Data.Context;
 using Api.Domain.Entities;
 using Api.Domain.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 
 namespace Api.Data.Repository
 {
     public class BaseRepository<T> : IRepository<T> where T : BaseEntity
     {
+        protected readonly MyContext _context;
+        private DbSet<T> _dbset;
+
+        public BaseRepository(MyContext myContext)
+        {
+            myContext = _context;
+            _dbset = _context.Set<T>();
+
+
+
+
+
+        }
         public Task<bool> DeleteAsync(T item)
         {
             throw new NotImplementedException();
         }
 
-        public Task<T> InsertAsync(T item)
+        public async Task<T> InsertAsync(T item)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (item.Id == Guid.Empty)
+                {
+                    item.Id = Guid.NewGuid();
+
+
+                }
+                item.CreatAt = DateTime.UtcNow;
+                _dbset.Add(item);
+                await _context.SaveChangesAsync();
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+
+            }
+
+            return item;
+
         }
 
         public Task<T> SelectAsync(Guid Id)
@@ -29,9 +67,27 @@ namespace Api.Data.Repository
             throw new NotImplementedException();
         }
 
-        public Task<T> UpdateAsync(T item)
+        public async Task<T> UpdateAsync(T item)
         {
-            throw new NotImplementedException();
+            item.CreatAt = DateTime.UtcNow;
+            var result = await _dbset.SingleOrDefaultAsync(p => p.Id.Equals(p.Id));
+            if (result == null)
+                return null;
+            item.CreatAt = result.CreatAt;
+
+            try
+            {
+                _context.Entry(result).CurrentValues.SetValues(item);
+                await _context.SaveChangesAsync();
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return item;
+
         }
     }
 }
